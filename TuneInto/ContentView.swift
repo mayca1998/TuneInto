2b22d713f620960027a767387cff015ff8bc023a
Maria del Carmen Hernandez <mariad@Marias-MBP.attlocal.net>
2020-07-21 19:22:59 -0700

    Adding Project

---
+++ TuneInto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TuneInto
+//
+//  Created by Maria del Carmen Hernandez on 7/21/20.
+//  Copyright © 2020 Maria del Carmen Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
